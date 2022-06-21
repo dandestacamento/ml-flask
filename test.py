33d296d8549f58b6a5c30b7629b83c2519482659
dandestacamento <dandestacamento@gmail.com>
2022-06-21 16:12:25 +1000

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from app import app
+
+def test1():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+
+
+def test2():
+    response = app.test_client().get("/base")
+    assert response.status_code == 200
+
+def test2():
+    response = app.test_client().get("/base")
+    assert b"My Daily checklist" in response.data
+    assert b"Added Task" in response.data
+    assert b"task 1" in response.data
